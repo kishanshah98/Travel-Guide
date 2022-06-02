cf3a1b0ac30b082e14a1d9f2fa2aade3955a5c70
Fat Tony <antdematteo8@gmail.com>
2022-06-02 11:28:05 -0400

    no changes made to file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,100 @@
-// var apiKey = "GmxPIEUb9z8zNRhQnxIXbycTDjTogloh";
-
-// $.ajax({
-//     type:"GET",
-//     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=GmxPIEUb9z8zNRhQnxIXbycTDjTogloh",
-//     async:true,
-//     dataType: "json",
-//     success: function(json) {
-//                 console.log(json);
-//                 // Parse the response.
-//                 // Do other things.
-//              }
-//   });
-
-
-// yelp api
-var apiKey = "rFlPqBV_66EyE8ZnW2gPlA1uKfHNFf8b9h-4yEQZuOdqSis4_VOBnA-jWORLf2oc_-DBUAdDK6tw3J6_rKR7P9ZJv-pFi76s9G5vPw72ppObfaA9YngRrix74DaWYnYx";
-var queryUrl = "https://api.yelp.com/v3/businesses/{id}/reviews";
-
-$.ajax({
-   url: queryUrl,
-   method: "GET"
-}).then(function(response) {
-   console.log(response);
-});
-// ==================================================================================================================================================
-
-// openweather api
-
 $(document).ready(function() {
-   // global variables
-   var searchBtn = $(".searchBtn");
-   var deleteBtn = $(".deleteBtn");
+    // Yelp API
+    var apiKey = "rFlPqBV_66EyE8ZnW2gPlA1uKfHNFf8b9h-4yEQZuOdqSis4_VOBnA-jWORLf2oc_-DBUAdDK6tw3J6_rKR7P9ZJv-pFi76s9G5vPw72ppObfaA9YngRrix74DaWYnYx";
+    var queryUrl = "https://api.yelp.com/v3/businesses/{id}/reviews";
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response);
+        var businessId = response.id;
+        console.log(businessId);
+    });
+
+// ==================================================================================================================================================
+   // OpenWeather API
+
+   // Global variables created
+   var searchBtn = $("#searchBtn");
+   var deleteBtn = $("#deleteBtn");
    var textArea = $("#textarea1");
-   var historyList = $(".history-list");
-   var currentWeatherDiv = $("#current-weather");
-   var forecastDiv = $("#forecast");
+   var cityList = $("#city-list");
+   var restaurantsDiv = $("#restaurants");
+   var weatherDiv = $("#weather");
+   var weatherCard = $("<div>").addClass("card");
+   
+   var title = $("<span>").addClass("card-title");
    // starting local storage
    var searchHistory = JSON.parse(window.localStorage.getItem("search-history")) || [];
 
+   // Retrieves the longitude and latitude required for the getCurrentWeather function
    function getGeoLocation () {
        var apiKey = "b0786aaf2595b4e2380f01ed8f03a7a4";
-       var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=1&appid=" + apiKey;
+       var searchInput = textArea.val().trim();
+       var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=1&appid=" + apiKey;
+       console.log(searchInput);
+       $.ajax({
+         url: url,
+         method: "GET"
+       }).then(function(response) {
+          console.log("Geolocation: ", response);
+          var lat = response[0].lat;
+          var lon = response[0].lon;
+        //   var weatherIcon = response.current.weather[0].icon;
+          console.log(lat, lon);
+          var name = response[0].name
+          title.text(response[0].name);
+          
+         
+          // temp, humidity, windspeed, icons
+        //   var cardIcon = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.current.weather[0].icon + ".png");
+        //   weatherCard.append(cardIcon);
+          
+          
+        //   weatherCard.append(cardIcon);
+
+          console.log(name);
+          weatherDiv.append(weatherCard);
+          getCurrentWeather(lat, lon);
+       });
    }
 
-   function getCurrentWeather(userInput) {
+   // Triggers getGeoLocation function to start once the search button is clicked
+   searchBtn.on("click", getGeoLocation);
+   searchBtn.on("click", historyList);
+
+   // Gets the weather forecast using longitude and latitude from getGeoLocation
+   function getCurrentWeather(lat, lon) {
        var apiKey = "b0786aaf2595b4e2380f01ed8f03a7a4";
-       var queryUrl = "https://api.openweathermap.org/data/2.5/onecall?=" + userInput + "lat={lat}&lon={lon}&exclude={part}&appid=" + apiKey + "&units=imperical";
+       var queryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
        $.ajax({
            url: queryUrl,
            method: "GET"
        }).then(function(response) {
-           console.log(response);
+           console.log("Current weather: ", response);
+           weatherCard.append(title);
+           for (var i = 0; i < 5; i++) {
+                var cardContent = $("<div>").addClass("card-content");
+                var temp = $("<p>").text("Current temperature (F): " + response.daily[i].temp.day);
+                var humidity = $("<p>").text("Current humidity: " + response.daily[i].humidity);
+                var windSpeed = $("<p>").text("Current wind speed (mph): " + response.daily[i].wind_speed);
+                cardContent.append(temp, humidity, windSpeed);
+                weatherCard.append(cardContent);
+                weatherDiv.append(weatherCard);
+           }
+           
+           
+        //    console.log(response.current.weather[0].icon);
+           
+           
        });
-       console.log("Search button clicked!!!");
    }
 
-   searchBtn.on("click", getCurrentWeather);
-
-
-
-
+   function historyList() {
+       var searchInput = textArea.val().trim();
+       var button = $("<button>").addClass("btn").text(searchInput);
+       cityList.append(button);
+   }
 });
+
